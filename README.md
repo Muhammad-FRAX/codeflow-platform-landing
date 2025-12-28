7467989b1081b5f0414604476dd5df848ed9a9b2
Mohamed Ali <mohammed44frx@gmail.com>
2025-12-28 20:19:39 +0200

    feature/ added the skeleton and structure of the app

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the link of the Guide Video that illustrates this application:
+
+            https://youtu.be/yS7B1W2SwaU?si=EtVcdgblDCLlo9TO
--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,12 +1,11 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import Pricing from './components/Pricing';
-import Testimonials from './components/Testimonials';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
       <Navbar />
@@ -16,7 +15,7 @@ function App() {
       <Testimonials />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
